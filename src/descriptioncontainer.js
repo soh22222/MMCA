9574156e038c8e221295cb06e7d5993613ecd9bc
Minhyeok <tracertw@gmail.com>
2023-08-28 22:00:39 +0900

    update start layout

--- src/descriptioncontainer.js
+++ src/descriptioncontainer.js
@@ -16,3 +16,28 @@ const closeStart = () => {
     const start = document.getElementById('start-layout')
     start.style.display = 'none'
 }
+
+const playLeft = () => {
+    const startContainer = document.getElementById('start-container')
+    startContainer.style.display = 'none'
+    const videoContainer = document.getElementById('selected-video')
+    videoContainer.style.display = 'flex'
+    videoContainer.style.width = '80%'
+    videoContainer.style.height = '80%'
+}
+
+const playRight = () => {
+    const startContainer = document.getElementById('start-container')
+    startContainer.style.display = 'none'
+    const videoContainer = document.getElementById('selected-video')
+    videoContainer.style.display = 'flex'
+    videoContainer.style.width = '95%'
+    videoContainer.style.height = '50%'
+}
+
+const backToStart = () => {
+    const videoContainer = document.getElementById('selected-video')
+    videoContainer.style.display = 'none'
+    const startContainer = document.getElementById('start-container')
+    startContainer.style.display = 'flex'
+}
