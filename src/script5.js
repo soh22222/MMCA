1ab938fce936f267e5a46699e3b4f90737a20819
Minhyeok <tracertw@gmail.com>
2023-08-30 22:40:01 +0900

    update video player

--- src/script5.js
+++ src/script5.js
@@ -443,7 +443,7 @@ const openPopup = (id) => {
         case 'vi':
             contents.innerHTML = `
             <div id="video-player">
-            <div id="content-video" style="padding:56.25% 0 0 0;position:relative;"><iframe id="video-frame" src="${videolinks[lr][contentId[0]]}" style="position:absolute;top:0;left:0;width:100%;max-height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+            <div id="content-video" style="padding:56.25% 0 0 0;position:relative;"><iframe id="video-frame" src="${videolinks[lr][contentId[0]]}" style="position:absolute;top:0;left:0;width:100%;height:100%;max-height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
             </div>`
             break
         case 'AD':
