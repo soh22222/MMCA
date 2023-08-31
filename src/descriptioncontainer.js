547eed9cc1d14cbc1167f8af29e5c22fd10d41b0
Minhyeok <tracertw@gmail.com>
2023-08-31 23:30:18 +0900

    test

--- src/descriptioncontainer.js
+++ src/descriptioncontainer.js
@@ -63,6 +63,7 @@ const playRight = () => {
     videoContainer.style.width = '95%'
     videoContainer.style.height = '50%'
     const video = document.getElementById('start-video')
+    video.contentWindow.location.replace('https://player.vimeo.com/video/859272684?h=c02df0c148')
     video.contentWindow.location.replace('https://player.vimeo.com/video/856273126?h=bde41651c9')
     document.body.dataset.lr = '1'
 }
